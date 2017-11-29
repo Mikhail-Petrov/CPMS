dcdeadb82796f363a52dcea3d1549fdaf7b8f844
Mikhail-Petrov <dragon294@mail.ru>
2017-11-29 11:45:47 +0300

    added field lighting when wrong spelling for tasks and profiles

--- src/main/webapp/resources/js/addCheck.js
+++ src/main/webapp/resources/js/addCheck.js
@@ -1,6 +1,6 @@
 function addCheck(element, min, max) {
 	var len = element.value.length;
-	if (len < min || len > max)
+	if (len < min || len > max && max > 0)
 		element.style.borderColor = 'red';
 	else
 		element.style.borderColor = '#d7d7d7';
